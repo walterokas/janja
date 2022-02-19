1d0202f25bea6e5caa8eed354fb6cff958997a81
Walter Okas <walter@dmarkmobile.com>
2022-02-20 00:19:10 +0300

    adding back wsgi.py

--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-import random
+import random, json
 
 from django.shortcuts import render
 from django.views import View
@@ -10,15 +10,22 @@ from api.models import JanjaSession
 
 BASE_URL = 'http://54.186.202.6:8888/ussd/255'
 # ?INPUT=255&SessionId=1645068896&MSISDN=256756666333&IMSI=641010247712386&TYPE=1'
-# Create your views here.
+
 class ApiEndpointView(View):
     def post(self, request, *args, **kwargs):
-        print(request.POST)
+        my_bytes_value = request.body
+        my_json = my_bytes_value.decode('utf8').replace("'", '"')
+        print('- ' * 20)
 
+        # Load the JSON to a Python list & dump it back out as formatted JSON
+        data = json.loads(my_json)
+        s = data
+        #s = json.dumps(data, indent=4, sort_keys=True)
+        print(s)
 
-        phone_number = request.POST.get('sender_id')
-        user_input = request.POST.get('user_response')
-        full_name = request.POST.get('full_name')
+        phone_number = s.get('sender_id')
+        user_input = s.get('user_response')
+        full_name = s.get('full_name')
 
         if user_input.lower() in ['menu', 'cancel', 'back', '255']:
             ussd_input = '255'
@@ -37,7 +44,7 @@ class ApiEndpointView(View):
 
         print(f'Sending USSD Req: {params}')
         resp = requests.get(BASE_URL, params=params)
-        print(f'UUSD Resp: {resp.text}')
+        print(f'USSD Resp: {resp.text}')
 
         response = send_im(phone_number, resp.text)
         print(f'Whatsapp resp: {response.text}')
